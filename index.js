cc3a8a5c477e7288d7f1f6d6dac7666d22b28376
geromuhaia <109484407+geromuhaia@users.noreply.github.com>
2023-10-02 19:53:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+
+$(document).ready(function () {
+    $('.menu-toggle').click(function () {
+        $('.menu-toggle').toggleClass('active');
+    })
+})
